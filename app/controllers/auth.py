4fbd5f7230179f680c186da775e6be12e551d2ad
unknown <mokazemaini84@gmail.com>
2025-04-02 18:07:32 +0330

    Implement login feature: update auth endpoints to use generate_password_hash and check_password_hash

--- app/controllers/auth.py
+++ app/controllers/auth.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, request, jsonify
 from app.models.user import User
-from app.extensions import db
+from app.extensions import db, bcrypt
 from flask_jwt_extended import create_access_token
-from flask_bcrypt import bcrypt
 from app.enums.userRole import UserRole
 
 auth_bp = Blueprint("auth", __name__)
@@ -10,18 +9,21 @@ auth_bp = Blueprint("auth", __name__)
 @auth_bp.route("/register", methods=["POST"])
 def register():
     data = request.get_json()
-    password = bcrypt(data['password'])
-    user = User(username=data["username"], password=password,role = UserRole.USER)
+    hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+    
+    user = User(username=data["username"], password=hashed_password, role=UserRole.USER)
     db.session.add(user)
     db.session.commit()
+    
     return jsonify({"message": "User created"}), 201
 
 @auth_bp.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
-    hashed_password = bcrypt(data['password'])
     user = User.query.filter_by(username=data["username"]).first()
-    if not user or user.password != hashed_password:
+
+    if not user or not bcrypt.check_password_hash(user.password, data['password']):
         return jsonify({"message": "Invalid credentials"}), 401
+    
     access_token = create_access_token(identity=user.id)
     return jsonify(access_token=access_token)

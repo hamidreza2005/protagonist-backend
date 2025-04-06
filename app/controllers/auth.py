accdeac556658e78bee45485cc9875b2ccb50cb8
Hamidreza Hassani <h.r.hassani2005@gmail.com>
2025-04-06 16:09:52 +0330

    fix bug

--- app/controllers/auth.py
+++ app/controllers/auth.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, request, jsonify
 from app.models.user import User
-from app.extensions import db
+from app.extensions import db,bcrypt
 from flask_jwt_extended import create_access_token
-from flask_bcrypt import bcrypt
 from app.enums.userRole import UserRole
 
 auth_bp = Blueprint("auth", __name__)
@@ -10,7 +9,7 @@ auth_bp = Blueprint("auth", __name__)
 @auth_bp.route("/register", methods=["POST"])
 def register():
     data = request.get_json()
-    password = bcrypt(data['password'])
+    password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
     user = User(username=data["username"], password=password,role = UserRole.USER)
     db.session.add(user)
     db.session.commit()
@@ -19,7 +18,7 @@ def register():
 @auth_bp.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
-    hashed_password = bcrypt(data['password'])
+    hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
     user = User.query.filter_by(username=data["username"]).first()
     if not user or user.password != hashed_password:
         return jsonify({"message": "Invalid credentials"}), 401

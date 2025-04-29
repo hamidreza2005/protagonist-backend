7c984e8331af099c99a738da1b18c145c2cd64cf
Hamidreza Hassani <h.r.hassani2005@gmail.com>
2025-04-29 15:42:08 +0330

    fix migration errors

--- app/models/post.py
+++ app/models/post.py
@@ -2,6 +2,7 @@ from app.extensions import db
 from datetime import datetime
 
 class Post(db.Model):
+    __tablename__ = "posts"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     content = db.Column(db.Text, nullable=False)

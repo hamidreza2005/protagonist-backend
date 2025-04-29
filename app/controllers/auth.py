b00cd84c3ff7828b1a09cfda0e723ffb86ff6228
unknown <mokazemaini84@gmail.com>
2025-04-29 20:06:13 +0330

    fix bugs in register method

--- app/controllers/auth.py
+++ app/controllers/auth.py
@@ -1,7 +1,8 @@
+import re
 from flask import Blueprint, request, jsonify
 from app.models.user import User
 from app.extensions import db, bcrypt, limiter
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, unset_jwt_cookies, set_access_cookies
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, unset_jwt_cookies
 from app.enums.userRole import UserRole
 from datetime import timedelta
 
@@ -11,12 +12,25 @@ auth_bp = Blueprint("auth", __name__)
 @limiter.limit("5/minute")
 def register():
     data = request.get_json()
-    password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
-    user = User(username=data["username"], password=password, role=UserRole.USER)
+
+    username = data.get("username", "")
+    password = data.get("password", "")
+
+    if not re.fullmatch(r"^[a-zA-Z0-9]{3,}$", username):
+        return jsonify({"message": "Username must be at least 3 characters and contain only letters and numbers."}), 400
+
+    if len(password) < 4:
+        return jsonify({"message": "Password must be at least 4 characters long."}), 400
+
+    if User.query.filter_by(username=username).first():
+        return jsonify({"message": "User already exists. Please login."}), 409
+
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+    user = User(username=username, password=hashed_password, role=UserRole.USER)
     db.session.add(user)
     db.session.commit()
-    return jsonify({"message": "User created"}), 201
 
+    return jsonify({"message": "User created"}), 201
 
 @auth_bp.route("/login", methods=["POST"])
 @limiter.limit("5/minute")
@@ -28,7 +42,7 @@ def login():
         return jsonify({"message": "Invalid credentials"}), 401
 
     remember = data.get("remember", False)
-    expires = timedelta(days=30 if remember else 1)
+    expires = timedelta(days=10 if remember else 1)
     additional_claims = {"role": user.role.value}
 
     access_token = create_access_token(
